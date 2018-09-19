64f3dcf04d49b5f975751b644466dc03a1a42f67
Doyeong Kim <doyeong.kim@cern.ch>
2018-09-19 09:13:50 +0200

    change: ROC producer for current formet of final_nominal.root
    - doc/task.txt has binning info of available distribution plots.

--- 2016SMHTT/tt/analysis/test/python/plotRocCurve.py
+++ 2016SMHTT/tt/analysis/test/python/plotRocCurve.py
@@ -14,63 +14,51 @@ ROOT.gROOT.SetBatch(True)
 ROOT.gStyle.SetOptStat(0)  
 
 ######## Load Files ######### 
-#fsignal = ROOT.TFile("files_nominal/ggH125.root") 
-#fztt = ROOT.TFile("files_nominal/ZTT.root") 
-file1 = ROOT.TFile("final_nominal_mjj.root")
-file2 = ROOT.TFile("final_nominal_MELA.root")
+file_mjj = ROOT.TFile("final_nominal_mjj.root")
+file_ggh = ROOT.TFile("final_nominal_normMELAggH.root")
+file_vbf = ROOT.TFile("final_nominal_normMELAvbf.root")
+file_vbfbyggh = ROOT.TFile("final_nominal_normMELAvbfbyggh.root")
+file_gghbyvbf = ROOT.TFile("final_nominal_normMELAgghbyvbf.root")
+
 
 
 ####### Produce ROC curve #######
-'''
 plotRocCurve_def.produce_roc_curve( 
-    file1,file2,file3, # input root files
-    'VBF125','VBF125_eff', # 'histogramName', ' legend Title' 
-    'ZTT','ZTT_eff', # 'histogramName', ' legend Title'
-    'mjj','MELA','NN',  # type
-    'ROC',
+    file_mjj,file_ggh,       # input root files
+    'ggH125','ggH125_eff',   # 'histogramName' in rootfile, 'legend Title' on the plot (x-axis)
+    'ZTT','ZTT_eff',         # 'histogramName' in rootfile, 'legend Title' on the plot (y-axis)
+    'mjj','MELA',            # category (legend name)
+    'ROC(normMELAggH)',      # output file name is 'ROC(normMELAggH).pdf'
     )
 
 plotRocCurve_def.produce_roc_curve( 
-    file,file2, # input root files
-    'VBF125_Dbkg_VBF','VBF125_eff', # 'histogramName', ' legend Title' 
-    'ZTT_Dbkg_VBF','ZTT_eff', # 'histogramName', ' legend Title'
-    'vbf','vbf',  # category
-    'ROC(Norm_Dbkg_VBF)',
+    file_mjj,file_vbf,       # input root files
+    'VBF125','VBF125_eff',   # 'histogramName' in rootfile, 'legend Title' on the plot (x-axis)
+    'ZTT','ZTT_eff',         # 'histogramName' in rootfile, 'legend Title' on the plot (y-axis)
+    'mjj','MELA',            # category (legend name)
+    'ROC(normMELAvbf)',      # output file name is 'ROC(normMELAvbf).pdf'
     )
-'''
-plotRocCurve_def.produce_roc_curve( 
-    file1,file2, # input root files
-    'ggH125','ggH125_eff', # 'histogramName', ' legend Title' 
-    'ZTT','ZTT_eff', # 'histogramName', ' legend Title'
-    'mjj','MELA',  # category
-    'ROC',
-    )
+
+
 
 ####### Produce MELA obs with ratio #######
-
 plotRocCurve_def.produce_ratio(
-    file3,
-    'NN_disc','vbf', # 'variable', 'category'
-    'VBF125','ZTT', # 'signal histo name', 'ztt histo name'
-    )
-'''
-plotRocCurve_def.produce_ratio(
-    file,
-    'Dbkg_VBF','2jets', # 'variable', 'category'
-    'VBF125_Dbkg_VBF','ZTT_Dbkg_VBF', # 'signal histo name', 'ztt histo name'
-    )
-
-plotRocCurve_def.produce_ratio(
-    file,
-    'Dbkg_ggH','vbf', # 'variable', 'category'
-    'ggH125_Dbkg_ggH','ZTT_Dbkg_ggH', # 'signal histo name', 'ztt histo name'
+    file_vbf,
+    'normMELAvbf','vbf',     # 'legend title', 'category' in root file
+    'VBF125','ZTT',          # 'signal histo name', 'ztt histo name'
     )
 plotRocCurve_def.produce_ratio(
-    file,
-    'Dbkg_ggH','2jets', # 'variable', 'category'
-    'ggH125_Dbkg_ggH','ZTT_Dbkg_ggH', # 'signal histo name', 'ztt histo name'
+    file_ggh,
+    'normMELAggh','vbf',     # 'legend title', 'category' in root file
+    'ggH125','ZTT',          # 'signal histo name', 'ztt histo name'
     )
-'''
-
-
-
+plotRocCurve_def.produce_ratio(
+    file_vbfbyggh,
+    'normMELAvbf_ggH','vbf',
+    'ggH125','VBF125',
+    )
+plotRocCurve_def.produce_ratio(
+    file_gghbyvbf,
+    'normMELAggH_vbf','vbf',
+    'ggH125','VBF125',
+    )
